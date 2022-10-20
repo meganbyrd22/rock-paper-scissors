060c6d257088b3b50de48c4e5734b34556f2f04d
Megan Byrd <meganbyrd22@gmail.com>
2022-10-20 13:07:18 -0500

    Add computer choices Add function for getComputerChoice

---
+++ RPS.js
@@ -0,0 +1,5 @@
+let computerChoices = ['Rock', 'Paper','Scissors']
+
+function getComputerChoice (computerChoices) {
+    return computerChoices [Math.floor.math.random() * computerChoices.lenth]
+}
