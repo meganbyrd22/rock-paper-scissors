d443729a50dfb4bd5536253b8e2d3a3f7bd36ac3
Megan Byrd <meganbyrd22@gmail.com>
2022-10-20 14:44:23 -0500

    Add strict equality marker instead of assigner

--- RPS.js
+++ RPS.js
@@ -14,8 +14,8 @@ function playRound (playerChoice, computerChoice) {
     if playerChoice === computerChoice {
         return "Tie!"
     else if playerChoice==='Rock' && computerChoice==='Scissor' ||
-    playerChoice='Paper' && computerChoice='Rock' ||
-    playerChoice='Scissor' && computerChoice='Paper'{
+    playerChoice==='Paper' && computerChoice==='Rock' ||
+    playerChoice==='Scissor' && computerChoice==='Paper'{
         return 'You win!'}
     else{ 
         return 'You lose!'
