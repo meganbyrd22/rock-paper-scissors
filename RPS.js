be36e5869f8b81fcb6b244a9a3a5a77d32b64700
Megan Byrd <meganbyrd22@gmail.com>
2022-10-20 14:39:16 -0500

    Add function for single round

--- RPS.js
+++ RPS.js
@@ -9,3 +9,16 @@ function playerChoice() {
     let playerChoice = prompt('Please choose rock, paper, or scissor.').toLowerCase();
     return playerChoice;
 }
+
+function playRound (playerChoice, computerChoice) {
+    if playerChoice === computerChoice {
+        return "Tie!"
+    else if playerChoice==='Rock' && computerChoice==='Scissor' ||
+    playerChoice='Paper' && computerChoice='Rock' ||
+    playerChoice='Scissor' && computerChoice='Paper'{
+        return 'You win!'}
+    else{ 
+        return 'You lose!'
+    }
+    }
+    }
