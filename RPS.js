1b6e1bb52dba584c2c1682dcf91e23ae45d40b27
Megan Byrd <meganbyrd22@gmail.com>
2022-10-20 15:14:18 -0500

    Remove function to define playerSelection

--- RPS.js
+++ RPS.js
@@ -5,22 +5,20 @@ function getComputerChoice () {
     return computerChoice;
 };
 
-function getPlayerChoice() {
-    let playerChoice = prompt('Please choose rock, paper, or scissor.').toLowerCase();
-    return playerChoice;
-}
+
+let playerChoice = prompt('Please choose rock, paper, or scissor.').toLowerCase();
+
 
 function playRound(playerChoice, computerChoice) {
     console.log(playerChoice);
     console.log(computerChoice);
     if playerChoice === computerChoice {
-        return "Tie!"
+        return "Tie!";}
     else if playerChoice==='Rock' && computerChoice==='Scissor' ||
     playerChoice==='Paper' && computerChoice==='Rock' ||
     playerChoice==='Scissor' && computerChoice==='Paper'{
         return 'You win!';}
     else{ 
         return 'You lose!';
-    }
     }
     }
