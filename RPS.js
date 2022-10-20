630f4fe581fcd9539316e95703e8d351997e2b8e
Megan Byrd <meganbyrd22@gmail.com>
2022-10-20 14:31:08 -0500

    Create function for playerChoice

--- RPS.js
+++ RPS.js
@@ -4,3 +4,8 @@ function getComputerChoice () {
     let computerChoice = choice [Math.floor (Math.random ()*  choices.lenth)]
     return computerChoice;
 };
+
+function playerChoice() {
+    let playerChoice = prompt('Please choose rock, paper, or scissor.').toLowerCase();
+    return playerChoice;
+}
