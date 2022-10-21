04196b2f0f319158f9437cf83d9b5328bf52e262
Megan Byrd <meganbyrd22@gmail.com>
2022-10-21 12:21:50 -0500

    Remove let from playerChoice declaration

--- RPS.js
+++ RPS.js
@@ -5,14 +5,13 @@ function getComputerChoice () {
     return computerChoice;
 };
 
-function playRound (playerChoice, computerChoice) {
-    let playerChoice = prompt("Please choose Rock, Paper, or Scissors.");
-
+function playRound(playerChoice, computerChoice) {
+    playerChoice = prompt("Please choose Rock, Paper, or Scissors.");
     if (playerChoice === computerChoice) {
         return "Tie!";}
     else if (playerChoice==='Rock' && computerChoice==='Scissor') ||
     (playerChoice==='Paper' && computerChoice==='Rock') ||
-    (playerChoice==='Scissor' && computerChoice==='Paper'){
+    (playerChoice==='Scissor' && computerChoice==='Paper')}
         return 'You win!';}
     else{ 
         return 'You lose!';
