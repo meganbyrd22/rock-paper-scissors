14e319609d54858090d53a6a76e324e19d675895
Megan Byrd <meganbyrd22@gmail.com>
2022-10-21 11:59:44 -0500

    Add () to aruments in else if statements

--- RPS.js
+++ RPS.js
@@ -10,9 +10,9 @@ function playRound (playerChoice, computerChoice) {
 
     if (playerChoice === computerChoice) {
         return "Tie!";}
-    else if playerChoice==='Rock' && computerChoice==='Scissor' ||
-    playerChoice==='Paper' && computerChoice==='Rock' ||
-    playerChoice==='Scissor' && computerChoice==='Paper'{
+    else if (playerChoice==='Rock' && computerChoice==='Scissor') ||
+    (playerChoice==='Paper' && computerChoice==='Rock') ||
+    (playerChoice==='Scissor' && computerChoice==='Paper'){
         return 'You win!';}
     else{ 
         return 'You lose!';
