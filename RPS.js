76a80f909b962f60205e28c427c4d56f7002e390
Megan Byrd <meganbyrd22@gmail.com>
2022-10-24 10:48:42 -0500

    Remove original playRound function

--- RPS.js
+++ RPS.js
@@ -3,25 +3,6 @@ let choices = ['Rock', 'Paper','Scissors'];
 function getComputerChoice () {
     let computerChoice = choice [Math.floor (Math.random ()*  choices.lenth)]
     return computerChoice;
-};
-
-function playRound(playerChoice, computerChoice) {
-    playerChoice = prompt("Please choose Rock, Paper, or Scissors.");
-    if (playerChoice === computerChoice) {
-        return "Tie!";}
-
-    else if (playerChoice==='Rock' && computerChoice ==='Scissor') {
-    return "You win!"}
-
-    else if (playerChoice ==='Paper' && computerChoice ==='Rock') {
-        return "You win!"}
-    
-    else if (playerChoice ==='Scissor' && computerChoice ==='Paper'){
-        return "You win!";}
-    else{ 
-        return "You lose!";
-    }
-    }
 
 for (let i = 0; i < 5; i++ ){
     let playerScore=0
