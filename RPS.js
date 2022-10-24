ea794b86515aee1be89c20a78af45db3ab21418a
Megan Byrd <meganbyrd22@gmail.com>
2022-10-24 13:22:45 -0500

    Re-add choices for getComputerChoice

--- RPS.js
+++ RPS.js
@@ -2,24 +2,24 @@ for (let i = 0; i < 5; i++ ){
     let playerScore=0
     let computerScore=0
     let roundWinner=''
+    let choices='Rock, Paper, Scissors'
 
     function getComputerChoice () {
         let computerChoice = choice [Math.floor (Math.random ()*  choices.lenth)]
         return computerChoice;
         
     function playRound           (playerChoice, computerChoice) {playerChoice = prompt("Please choose Rock, Paper, or Scissors.");
-        if (playerChoice === computerChoice) {
-            roundWinner='tie'}
+        if (playerChoice === computerChoice) {roundWinner='tie'}
     
-            else if (playerChoice==='Rock' && computerChoice ==='Scissor') {
+        else if (playerChoice==='Rock' && computerChoice ==='Scissor') {
             roundWinner='player'}
     
-            else if (playerChoice ==='Paper' && computerChoice ==='Rock') {
+        else if (playerChoice ==='Paper' && computerChoice ==='Rock') {
             roundWinner='player'}
         
-            else if (playerChoice ==='Scissor' && computerChoice ==='Paper'){
+        else if (playerChoice ==='Scissor' && computerChoice ==='Paper'){
             roundWinner='player';}
-            else{ 
+        else{ 
             roundWinner='computer';
         }
         }
