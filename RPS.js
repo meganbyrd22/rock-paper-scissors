fbaa25bad967085465e759b7d5a076297c85e5f3
Megan Byrd <meganbyrd22@gmail.com>
2022-10-26 12:58:20 -0500

    Move loop into updateScore Remove game function

--- RPS.js
+++ RPS.js
@@ -41,12 +41,10 @@ function showWinner() {
     else return('Tie, no one scores!')}
 
 function updateScore(){ 
+    for (let i = 0; i < 5; i++)
     {if  (showWinner ==='You win!'){ (playerScore)++}
     else if (showWinner==='You lose!'){(computerScore)++}
     return ('Player: ${playerScore}','Computer: ${computerScore}')
     }
-
-function game(){
-    for (let i = 0; i < 5; i++)
-
-
+    
+    
