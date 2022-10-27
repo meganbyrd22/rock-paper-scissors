57e9ee14576a345730a417b7e8c8731e01d0fe13
Megan Byrd <meganbyrd22@gmail.com>
2022-10-27 15:37:37 -0500

    Remove toString from game

--- RPS.js
+++ RPS.js
@@ -49,5 +49,5 @@ function updateScore(){
 
 function game(){
     if (updateScore= (playerScore)++ || (computerScore)++)
-    return (toString)(document.getElementByID('Player Score:'(playerScore))) |(document.getElementById('Computer:' (computerScore)))
+    return (document.getElementByID('Player Score:'(playerScore))) |(document.getElementById('Computer:' (computerScore)))
 }
