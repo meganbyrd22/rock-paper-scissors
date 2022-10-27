f6bdebf272095a6f63ff48d71eadbb43a7b38970
Megan Byrd <meganbyrd22@gmail.com>
2022-10-27 15:30:27 -0500

    Capitalize b in getElementById

--- RPS.js
+++ RPS.js
@@ -49,5 +49,5 @@ function updateScore(){
 
 function game(){
     if (updateScore= (playerScore)++ || (computerScore)++)
-    return (toString)(document.getElementbyID('Player Score:'(playerScore))) |(document.getElementById('Computer:' (computerScore)))
+    return (toString)(document.getElementByID('Player Score:'(playerScore))) |(document.getElementById('Computer:' (computerScore)))
 }
