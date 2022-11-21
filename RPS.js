3e9775bcdb1835a0c6a41fbf81efc24e8ab50128
Megan Byrd <meganbyrd22@gmail.com>
2022-11-21 15:13:35 -0600

    Add return calls to all functions again

--- RPS.js
+++ RPS.js
@@ -18,16 +18,7 @@ btnPaper.addEventListener('click', ()=> {
 btnScissors.addEventListener('click', () => {
    playerChoice = 'SCISSORS';
    playRound (playerChoice, computerChoice)})
-    
-//get playerChoice from results of eventListener
- //function handleClick(){
-    //playerChoice = (e);
-   // playRound(playerChoice, computerChoice)}
-    //(playerScore < 5 && computerScore < 5)
-    //return playerChoice;
-    //else return 'Game over!' };
  
-
 //get computer choice from random selection
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3)
@@ -44,15 +35,15 @@ function getComputerChoice() {
 //play one round    
 function playRound(computerChoice, playerChoice){
     for (let i = 0; i < 5; i++)
-    {if (playerChoice === computerChoice) { roundWinner ='tie'}
+    {if (playerChoice === computerChoice) {return roundWinner ='tie'}
     
     else if 
     ((playerChoice==='ROCK' && computerChoice ==='SCISSORS') || 
     (playerChoice ==='PAPER' && computerChoice ==='ROCK') || 
     (playerChoice ==='SCISSORS' && computerChoice ==='PAPER'))  
-    {roundWinner ='player'}
+    {return roundWinner ='player'}
     
-    else {roundWinner ='computer'};
+    else {return roundWinner ='computer'};
     }
     showWinner(roundWinner);
 }
@@ -60,20 +51,21 @@ function playRound(computerChoice, playerChoice){
 //print a winner message 
 function showWinner()
     {if (roundWinner ==='player'){
-       return winnerMessage = "You win!";
+        return winnerMessage = "You win!";
     }
     else if (roundWinner ==='computer'){
-        return winnerMessage ="You lose!";
+         return winnerMessage ="You lose!";
     }
+    else {return winnerMessage ="It's a tie!";
+    }
+    updateScore(winnerMessage);
+}
 
-    else {return winnerMessage.textContent="It's a tie!";
-}
-}
 //update the score + display current score
 function updateScore(){ 
     {if  (winnerMessage ==='You win!'){ (playerScore)++}
     else if (winnerMessage ==='You lose!'){(computerScore)++}
     }
-    playerScoreText.textContent = `Player: ${playerScore}`,computerScoreText.textContent= `Computer:${computerScore}`;
+    return playerScoreText.textContent = `Player: ${playerScore}`,computerScoreText.textContent= `Computer:${computerScore}`;
 }
 
