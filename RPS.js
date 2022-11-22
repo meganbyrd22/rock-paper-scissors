d70bc50ba7b7282658f20586546d1f9d6d6b2e02
Megan Byrd <meganbyrd22@gmail.com>
2022-11-22 10:17:48 -0600

    Add " " to computerScoreText

--- RPS.js
+++ RPS.js
@@ -12,7 +12,7 @@ const btnPaper = document.getElementById("btnPaper")
 const btnScissors = document.getElementById("btnScissors")
 const scoreboard = document.getElementsByClassName ("scoreboard")
 const playerScoreText = document.getElementById("playerScoreText")
-const computerScoreText = document.getElementById(computerScoreText)
+const computerScoreText = document.getElementById("computerScoreText")
 const scoreMessage = document.getElementsByClassName("scoreMessage")
 
 // get playerChoice from button and initialize playRound 
